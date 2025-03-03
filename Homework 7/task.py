1dc22ae5929b7a123ae391a8781e0ca3e0d736fa
Yana <yana.salova.02@gmail.com>
2025-03-04 00:45:54 +0300

    hw 7

--- Homework 7/task.py
+++ Homework 7/task.py
@@ -1,6 +1,5 @@
 import numpy as np
 import copy
-import cvxopt
 from cvxopt import spmatrix, matrix, solvers
 from sklearn.datasets import make_classification, make_moons, make_blobs
 from typing import NoReturn, Callable
@@ -37,37 +36,24 @@ class LinearSVM:
             (можно считать, что равны -1 или 1). 
         
         """
-
-        y = y.astype(float)
-        n_samples, n_features = X.shape
-        K = np.dot(X, X.T) 
-        Q = (y[:, None] * y[None, :]) * K
-        P = matrix(Q, tc='d')
-        q = matrix(-1.0 * np.ones(n_samples), tc='d')
-        G_std = np.eye(n_samples) * -1.0  
-        G_slack = np.eye(n_samples)       
-        G = np.vstack((G_std, G_slack))
-        h_std = np.zeros(n_samples)       
-        h_slack = np.ones(n_samples) * self.C
-        h = np.concatenate((h_std, h_slack))
-        G = matrix(G, tc='d')
-        h = matrix(h, tc='d')
-        A = matrix(y.reshape(1, -1), tc='d')
-        b = matrix(np.zeros(1), tc='d')
-
-   
-        cvxopt.solvers.options['show_progress'] = False  
+        n = X.shape[0]
+        tmp = y.reshape(-1, 1) * X
+        P = matrix(tmp @ tmp.T)
+        q = matrix(-np.ones((n, 1)))
+        G = matrix(np.vstack((-np.eye(n), np.eye(n))))
+        h = matrix(np.hstack((np.zeros(n), self.C * np.ones(n))))
+        A = matrix(y.reshape(1, -1) * 1.)
+        b = matrix(0.)
+        
         sol = solvers.qp(P, q, G, h, A, b)
-        alphas = np.ravel(sol['x'])
-        self.w = ((alphas * y)[:, None] * X).sum(axis=0)
-        eps = 1e-7
-        self.support = np.where(alphas > eps)[0]
-        support_non_sat = np.where((alphas > eps) & (alphas < self.C - eps))[0]
-        if len(support_non_sat) > 0:
-            self.b = np.mean([y[i] - np.dot(self.w, X[i]) for i in support_non_sat])
-        else:
-            self.b = np.mean([y[i] - np.dot(self.w, X[i]) for i in self.support])
+        alpha = np.array(sol['x']).flatten()  
+        threshold = 1e-5
+        support = alpha > threshold
+        self.support = np.where(support)[0]
+        self.w = np.sum(alpha[support, None] * y[support, None] * X[support], axis=0)
+        self.b = np.mean(y[support] - X[support] @ self.w)
 
+    
     def decision_function(self, X: np.ndarray) -> np.ndarray:
         """
         Возвращает значение решающей функции.
@@ -84,7 +70,8 @@ class LinearSVM:
             (т.е. то число, от которого берем знак с целью узнать класс).     
         
         """
-        return X.dot(self.w) + self.b
+
+        return X@self.w.T+self.b
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -102,26 +89,26 @@ class LinearSVM:
         
         """
         return np.sign(self.decision_function(X))
-    
+
 # Task 2
 
 def get_polynomial_kernel(c=1, power=2):
-    """
-    Возвращает полиномиальное ядро с заданной константой и степенью.
-    """
-    def kernel(X1, X2):
-        return (X1 @ X2.T + c) ** power 
+    "Возвращает полиномиальное ядро с заданной константой и степенью"
+    def kernel(X,Y):
+        return (X@Y.T+c)**power
+
     return kernel
 
 def get_gaussian_kernel(sigma=1.):
-    """
-    Возвращает ядро Гаусса с заданным коэффицинтом сигма
-    """
-    def kernel(x,y):
-        return np.exp(-sigma * np.linalg.norm(x[:, None, :] - y[None, :, :], axis=-1) ** 2)
-    
+    "Возвращает ядро Гаусса с заданным коэффицинтом sigma"
+    def kernel(x, y):
+        x = np.atleast_2d(x)
+        y = np.atleast_2d(y)
+        result = np.exp(-sigma * np.linalg.norm(x[:, None, :] - y[None, :, :], axis=-1)**2)
+        if x.shape[0] == 1 or y.shape[0] == 1:
+            result = np.squeeze(result)
+        return result
     return kernel
-
 
 
 # Task 3
@@ -129,6 +116,7 @@ def get_gaussian_kernel(sigma=1.):
 class KernelSVM:
     def __init__(self, C: float, kernel: Callable):
         """
+        
         Parameters
         ----------
         C : float
@@ -140,10 +128,6 @@ class KernelSVM:
         self.C = C
         self.kernel = kernel
         self.support = None
-        self.alpha = None
-        self.b = None
-        self.X_s = None
-        self.y = None
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
         """
@@ -170,8 +154,8 @@ class KernelSVM:
 
         sol=solvers.qp(P,q,G,h,A,b)
         alpha=np.array(sol['x']).flatten()
-        eps = 1e-5
-        self.support=np.where(alpha>eps)[0]
+        threshold = 1e-5
+        self.support=np.where(alpha>threshold)[0]
         
         self.alpha=alpha.reshape(-1,1)
         self.X_s = X[self.support]
@@ -195,6 +179,7 @@ class KernelSVM:
             (т.е. то число, от которого берем знак с целью узнать класс).     
         
         """
+    
         Xw = (self.alpha[self.support] * self.y[self.support]).T @ self.kernel(self.X_s, X)  
         return Xw.flatten() + self.b
 
