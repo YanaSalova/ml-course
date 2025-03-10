fc7957c4d0f9ca2c0521524aa415fc4459c80464
Yana <yana.salova.02@gmail.com>
2025-03-10 14:46:32 +0300

    hw 7

--- Homework 7/task.py
+++ Homework 7/task.py
@@ -116,14 +116,12 @@ def get_gaussian_kernel(sigma=1.):
 class KernelSVM:
     def __init__(self, C: float, kernel: Callable):
         """
-        
         Parameters
         ----------
         C : float
             Soft margin coefficient.
         kernel : Callable
             Функция ядра.
-        
         """
         self.C = C
         self.kernel = kernel
@@ -131,72 +129,42 @@ class KernelSVM:
 
     def fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
         """
-        Обучает SVM, решая задачу оптимизации при помощи cvxopt.solvers.qp
-        
-        Parameters
-        ----------
-        X : np.ndarray
-            Данные для обучения SVM.
-        y : np.ndar/ray
-            Бинарные метки классов для элементов X 
-            (можно считать, что равны -1 или 1). 
-        
+        Обучает SVM, решая задачу оптимизации с помощью cvxopt.solvers.qp.
         """
+        n = X.shape[0]
+        self.y = y.reshape(-1, 1)
+        K = self.kernel(X, X)
+        
 
-        n=X.shape[0]
-        self.y=y.reshape(-1,1)
-        P=matrix(self.y@self.y.T * self.kernel(X, X))
-        q=matrix(-np.ones((n,1)))
-        G=matrix(np.vstack((-np.eye(n),np.eye(n))))
-        h=matrix(np.hstack((np.zeros(n),self.C*np.ones(n))))
-        A=matrix(y.reshape(1,-1)*1.)
-        b=matrix(0.)
+        P = matrix(np.multiply(self.y @ self.y.T, K))
+        
+        q = matrix(-np.ones((n, 1)))
+        G = matrix(np.vstack((-np.eye(n), np.eye(n))))
+        h = matrix(np.hstack((np.zeros(n), self.C * np.ones(n))))
+        A = matrix(y.reshape(1, -1).astype(np.double))
+        b = matrix(0.0)
 
-        sol=solvers.qp(P,q,G,h,A,b)
-        alpha=np.array(sol['x']).flatten()
+        sol = solvers.qp(P, q, G, h, A, b)
+        alpha = np.array(sol['x']).flatten()
         threshold = 1e-5
-        self.support=np.where(alpha>threshold)[0]
+        self.support = np.where(alpha > threshold)[0]
         
-        self.alpha=alpha.reshape(-1,1)
+        self.alpha = alpha.reshape(-1, 1)
         self.X_s = X[self.support]
-        self.b = np.mean(self.y[self.support] - np.sum(self.alpha[self.support] * self.y[self.support] * self.kernel(self.X_s, self.X_s), axis=0))
-
+        self.alpha_y = (self.alpha * self.y)[self.support]
         
+        K_ss = self.kernel(self.X_s, self.X_s)
+        self.b = np.mean(self.y[self.support] - np.sum(self.alpha_y * K_ss, axis=0))
 
     def decision_function(self, X: np.ndarray) -> np.ndarray:
         """
-        Возвращает значение решающей функции.
-        
-        Parameters
-        ----------
-        X : np.ndarray
-            Данные, для которых нужно посчитать значение решающей функции.
-
-        Return
-        ------
-        np.ndarray
-            Значение решающей функции для каждого элемента X 
-            (т.е. то число, от которого берем знак с целью узнать класс).     
-        
+        Возвращает значение решающей функции для каждого элемента X.
         """
-    
-        Xw = (self.alpha[self.support] * self.y[self.support]).T @ self.kernel(self.X_s, X)  
-        return Xw.flatten() + self.b
-
+        K_test = self.kernel(self.X_s, X)
+        return (self.alpha_y.T @ K_test).flatten() + self.b
 
     def predict(self, X: np.ndarray) -> np.ndarray:
         """
         Классифицирует элементы X.
-        
-        Parameters
-        ----------
-        X : np.ndarray
-            Данные, которые нужно классифицировать
-
-        Return
-        ------
-        np.ndarray
-            Метка класса для каждого элемента X.   
-        
         """
         return np.sign(self.decision_function(X))
